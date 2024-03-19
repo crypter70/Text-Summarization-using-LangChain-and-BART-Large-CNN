828745601f6e59241bd59031ec775f3f83e210c4
crypter70 <yosafatcrypter@gmail.com>
2024-03-19 20:36:26 +0700

    Add notebook and Python file

---
+++ App.py
@@ -0,0 +1,54 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.docstore.document import Document
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.document_loaders import WebBaseLoader
+from dotenv import load_dotenv
+import streamlit as st
+
+
+load_dotenv()
+
+repo_id = "facebook/bart-large-cnn"
+llm = HuggingFaceHub(
+    repo_id=repo_id, 
+    model_kwargs={"temperature":0.5, "max_length":1000}, 
+)
+text_splitter = CharacterTextSplitter()
+
+
+def preprocessing_text(text_splitter, input_text):
+    texts = text_splitter.split_text(input_text)
+    docs = [Document(page_content=t) for t in texts]
+    return docs
+
+
+def summarize_text(llm, docs):
+    chain = load_summarize_chain(llm, chain_type="stuff", verbose=True)
+    summary = chain.invoke(docs)
+    return summary['output_text']
+
+
+def main():
+    st.title("Summarize My Sentences")
+
+    with st.form(key="summarize_form"):
+        input_text = st.text_area('Enter the text to summarize (minimum 600 characters):')
+        preprocessed_text = preprocessing_text(text_splitter, input_text)
+
+        submitted = st.form_submit_button("Summarize")
+
+        if submitted and len(input_text) >= 600:
+            with st.spinner('Summarizing sentences...'):
+                summary = summarize_text(llm, preprocessed_text)
+            st.subheader("Summary:")
+            st.write(summary)
+        elif submitted and len(input_text) < 600:
+            st.error("Please enter at least 600 characters of text.")
+        elif not submitted and len(input_text) < 600:
+            st.info("Minimum 600 characters of text required to summarize.")
+    
+
+if __name__ == "__main__":
+    main()
+
